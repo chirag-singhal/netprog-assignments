52f87504b1a0a433460febecbe9c332e2cbcb454
CHIRAG SINGHAL <csinghal208@gmail.com>
2021-03-28 07:48:26 +0000

    bg proc check

--- shell.c
+++ shell.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/signal.h>
+#include <sys/wait.h>
 #include <errno.h>
 
 #define MAX_CMD_LEN 512
 
-const char * PATH;
+char * PATH;
 
 void err_exit(const char *err_msg) {
     perror(err_msg);
     exit(EXIT_FAILURE);
 }
 
-char **parser_cmd(const char *cmd ) {
+char **parser_cmd(char *cmd ) {
     const char delim[]= " ";
     int size = 0;
     char temp[MAX_CMD_LEN][MAX_CMD_LEN];
@@ -37,9 +39,9 @@ char **parser_cmd(const char *cmd ) {
     return cmd_toks;
 }
 
-void execute_cmd(const char **cmd_toks, int n_cmd_toks) {
+void execute_cmd(char **cmd_toks, int n_cmd_toks) {
     // Search in PATH for command
-    char cmd_path[MAX_CMD_LEN] = NULL;
+    char cmd_path[MAX_CMD_LEN];
     
     char * token = strtok(PATH, ":");
     while (token != NULL) {
@@ -73,11 +75,26 @@ void execute_cmd(const char **cmd_toks, int n_cmd_toks) {
 int main() {
 
     PATH = getenv("PATH");
-    char cmd[MAX_CMD_LEN];
+    char cmd[MAX_CMD_LEN + 1];
 
     while (1) {
         printf(">> ");
-        fgets(cmd, MAX_CMD_LEN, stdin);
+        char *cmd_read = fgets(cmd, MAX_CMD_LEN + 1, stdin);
+        
+        // If no char read or error occured 
+        if(cmd_read == NULL || cmd[0] == '\n')
+            continue;
+
+        int cmd_len = strlen(cmd_read);
+        cmd_read[cmd_len - 1] = '\0';
+        bool is_bg_proc = false;
+
+        if(cmd_read[cmd_len - 2] == '&') {
+            is_bg_proc = true;
+            cmd_read[cmd_len - 2] = '\0';
+        } 
+
+        cmd_len = strlen(cmd_read);
 
         // Spawn a new process group for the `cmd`
         //
