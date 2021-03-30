a0b886f2c613174518deff3d80392f771c58f8b4
CHIRAG SINGHAL <csinghal208@gmail.com>
2021-03-30 21:38:43 +0000

    P3 client done server left

--- P3/msgq_client.c
+++ P3/msgq_client.c
@@ -4,30 +4,243 @@
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <pthread.h>
 
-#define MAX_CMD_LEN 32
-#define ALIAS_NAME_SIZE 20
+#define MAX_CMD_LEN 50
+#define MAX_NAME_LEN 30
+#define MAX_GROUP_SIZE 32
+#define MAX_NUM_GROUPS 64
+#define MAX_OLD_MSG 128
+#define MAX_MSG_SIZE 4096
+
+typedef enum _MSG_TYPE {
+    PRIVATE_MSG,
+    GROUP_MSG,
+    CREATE_GROUP_MSG,
+    LIST_GROUP_MSG,
+    JOIN_GROUP_MSG,
+    AUTO_DELETE_MSG
+} MSG_TYPE;
+
+typedef struct _MSG {
+    MSG_TYPE type;
+    char sender[MAX_NAME_LEN];
+    char receiver[MAX_NAME_LEN];
+    char body[MAX_MSG_SIZE];
+    char group[MAX_NAME_LEN];
+    time_t time;
+    int delete_time;
+} MSG;
+
+int msg_id;
+char* user_name;
+pthread_mutex_t lock;
+int is_rcvd_mssg = 0;
 
 
-void prompt(char* alias_name) {
-    printf("[%s] ", alias_name);
+void prompt() {
+    printf("[%s] ", user_name);
+}
+
+void err_exit(const char *err_msg) {
+    printf("%s\n", err_msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_mssg(MSG* msg) {
+    if(msgsnd(msg_id, msg, sizeof(MSG), 0) < 0)
+        err_exit("Error sending message to server. Exiting...");
+}
+
+void* rcv_mssg() {
+    
+    MSG* msg = malloc(sizeof(MSG));
+
+    key_t key_client = ftok(user_name, 'z');
+    int msg_id_client = msgget(key_client, 0644 | IPC_CREAT);
+
+    if(msg_id < 0) {
+        err_exit("Error while creating message queue. Exiting...");
+    }
+
+    while(true) {
+        pthread_mutex_lock(&lock);
+
+        if(msgrcv(msg_id_client, msg, sizeof(MSG), 0, IPC_NOWAIT) < 0) {
+            //unlock and sleep
+            pthread_mutex_unlock(&lock);
+            sleep(0.2);
+            continue;
+        }
+        if(msg -> type == GROUP_MSG) {
+            printf("[group][%s] %s\n", msg -> group, msg -> body);
+        }
+        else if(msg -> type == PRIVATE_MSG) {
+            printf("[pvt][%s] %s\n", msg -> sender, msg -> body);
+        }
+        else if(msg -> type == LIST_GROUP_MSG) {
+            printf("***************\n");
+            printf("Available groups to join\n");
+            printf("%s\n", msg -> body);
+            printf("***************\n");
+            is_rcvd_mssg = 1;
+        }
+        pthread_mutex_unlock(&lock);
+    }
+    msgctl(msg_id_client, IPC_RMID, 0);
+}
+
+void create_group(char* group_name) {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = CREATE_GROUP_MSG;
+    strcpy(msg -> sender, user_name);
+    strcpy(msg -> group, group_name);
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
+}
+
+void join_group(char *group_name) {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = JOIN_GROUP_MSG;
+    strcpy(msg -> sender, user_name);
+    strcpy(msg -> group, group_name);
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
+}
+
+void list_groups() {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = LIST_GROUP_MSG;
+    strcpy(msg -> sender, user_name);
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
+    while(!is_rcvd_mssg) {
+        sleep(0.2);
+    }
+    is_rcvd_mssg = 0;
+}
+
+void send_group_mssg(char* group_name, char* mssg) {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = GROUP_MSG;
+    strcpy(msg -> sender, user_name);
+    strcpy(msg -> body, mssg);
+    strcpy(msg -> group, group_name);
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
+}
+
+void send_priv_mssg(char* rcvr_name, char* mssg) {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = PRIVATE_MSG;
+    strcpy(msg -> sender, user_name);
+    strcpy(msg -> receiver, rcvr_name);
+    strcpy(msg -> body, mssg);
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
+}
+
+void set_auto_delete(char* group_name, int time_sec) {
+    MSG* msg = malloc(sizeof(MSG));
+    msg -> type = AUTO_DELETE_MSG;
+    strcpy(msg -> sender, user_name);
+    strcpy(msg -> group, group_name);
+    msg -> delete_time = time_sec;
+    send_mssg(msg);
+    pthread_mutex_unlock(&lock);
 }
 
 int main() {
 
-    char* alias_name = malloc(sizeof(char) * ALIAS_NAME_SIZE);
+    key_t key = ftok("NETPROG", 'z');
+    msg_id = msgget(key, 0666 | IPC_CREAT);
+
+    if(msg_id < 0) {
+        err_exit("Error while creating message queue. Exiting...");
+    }
+
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        err_exit("\n Error Mutex init. Exiting...\n");
+    }
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, rcv_mssg, NULL);
+
+    user_name = malloc(sizeof(char) * MAX_NAME_LEN);
     printf("Enter alias name : ");
-    size_t max_alias_len = ALIAS_NAME_SIZE;
-    size_t alias_len = getline(&alias_name, &max_alias_len, stdin);
-    alias_name[alias_len - 1] = '\0';
+    size_t max_name_len = MAX_NAME_LEN;
+    size_t alias_len = getline(&user_name, &max_name_len, stdin);
+    user_name[alias_len - 1] = '\0';
 
     while(true) {
-        prompt(alias_name);
+        
+        prompt();
+
         char * cmd = malloc(sizeof(char) * (MAX_CMD_LEN + 1));
         size_t max_cmd_len = MAX_CMD_LEN;
         size_t cmd_len = getline(&cmd, &max_cmd_len, stdin);
+        pthread_mutex_lock(&lock);
         cmd[cmd_len - 1] = '\0';
         cmd_len = strlen(cmd);
+
+        char * tmp_cmd = strdup(cmd);
+        char* saved_ptr;
+        char* token = strtok_r(tmp_cmd, " ", &saved_ptr);
+        
+        if(strcmp(token, "create") == 0) {
+            //create group
+            token = strtok_r(NULL, " ", &saved_ptr);
+            create_group(token);
+        }
+        else if(strcmp(token, "join") == 0) {
+            //join group
+            token = strtok_r(NULL, " ", &saved_ptr);
+            join_group(token);
+        }
+        else if(strcmp(token, "list") == 0) {
+            //list groups
+            list_groups();
+        }
+        else if(strcmp(token, "send") == 0) {
+            //send -p (private) -g (group)
+            token = strtok_r(NULL, " ", &saved_ptr);
+            if(strcmp(token, "-p") == 0) {
+                //private mssg send
+                token = strtok_r(NULL, " ", &saved_ptr);
+                char *mssg = token + strlen(token) + 1;
+                send_group_mssg(token, mssg);
+            }
+            else if(strcmp(token, "-g") == 0) {
+                //group mssg send
+                token = strtok_r(NULL, " ", &saved_ptr);
+                char *mssg = token + strlen(token) + 1;
+                send_priv_mssg(token, mssg);
+            }
+            else {
+                //invalid command
+                err_exit("Error: invalid command. Exiting...");
+            }
+        }
+        else if(strcmp(token, "auto") == 0) {
+            //auto delete
+            token = strtok_r(NULL, " ", &saved_ptr);
+            if(strcmp(token, "delete") == 0) {
+                //private mssg send
+                char* group_name = strtok_r(NULL, " ", &saved_ptr);
+                token = strtok_r(NULL, " ", &saved_ptr);
+                int time_sec = atoi(token);
+                set_auto_delete(group_name, time_sec);
+            }
+        }
+        else {
+            //invalid command
+            err_exit("Error: invalid command. Exiting...");
+        }
+
     }
+    pthread_join(thread_id, NULL);
     return EXIT_SUCCESS;
 }
--- P3/msgq_server.c
+++ P3/msgq_server.c
@@ -1,33 +1,222 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#define SENDER_NAME_MAX 30
-#define MSG_SIZE_MAX 256
-#define MAX_GROUP_SIZE 32;
+#define MAX_NAME_LEN 30
+#define MAX_GROUP_SIZE 32
+#define MAX_NUM_GROUPS 64
+#define MAX_OLD_MSG 128
+#define MAX_MSG_SIZE 2048
+
+typedef enum _MSG_TYPE {
+    PRIVATE_MSG,
+    GROUP_MSG,
+    CREATE_GROUP_MSG,
+    LIST_GROUP_MSG,
+    JOIN_GROUP_MSG,
+    AUTO_DELETE_MSG
+} MSG_TYPE;
 
 typedef struct _MSG {
-    long int msg_type;
-    char sender[SENDER_NAME_MAX];
-    char msg_body[MSG_SIZE_MAX];
-    int time;
+    MSG_TYPE type;
+    char sender[MAX_NAME_LEN];
+    char receiver[MAX_NAME_LEN];
+    char body[MAX_MSG_SIZE];
+    char group[MAX_NAME_LEN];
+    time_t time;
+    time_t delete_time;
 } MSG;
 
+typedef struct _OLD_MSG {
+    MSG msg[MAX_OLD_MSG];
+    int start_ptr;
+    size_t n_msg;
+} OLD_MSG;
+
 typedef struct _GROUP {
-    char name[SENDER_NAME_MAX];
-    char ip[INET_ADDRSTRLEN];
-    int send_fd;
-	struct sockaddr_in send_addr;
-	int recv_fd;
-	struct sockaddr_in recv_addr;
-    char members[][INET_ADDRSTRLEN];
+    char name[MAX_NAME_LEN];
+    size_t n_members;
+    char members[MAX_GROUP_SIZE][MAX_NAME_LEN];
+    time_t join_time[MAX_GROUP_SIZE];
+    OLD_MSG old_msg;
+    time_t delete_time;
 } GROUP;
 
-int main() {
+size_t N_GROUPS;
+GROUP ALL_GROUPS[MAX_NUM_GROUPS];
 
+int get_old_msg(OLD_MSG * old_msg, size_t pos) {
+    if (old_msg->n_msg == 0)
+        return -1;
     
+    return (old_msg->start_ptr + pos - 1) % MAX_OLD_MSG;
+}
+
+int add_old_msg(OLD_MSG * old_msg, MSG * msg) {
+    if (old_msg->n_msg == MAX_OLD_MSG) {
+        printf("Maximum number of old messages...\n");
+        return -1;
+    }
+    
+    if (old_msg->n_msg == 0) {
+        old_msg->start_ptr = 0;
+        old_msg->n_msg++;
+        old_msg->msg[0] = *msg;
+        return 0;
+    }
+
+    old_msg->msg[(old_msg->start_ptr + old_msg->n_msg)] = *msg;
+    old_msg->n_msg++;
+    return 0;
+}
+
+int get_queue_id(const char * username) {
+    key_t id = ftok(username, 'z');
+    return msgget(id, IPC_CREAT|0666);
+}
+
+int join_group(char * groupname, char * username) {
+    GROUP * grp = NULL;
+    for (size_t grp_idx = 0; grp_idx < N_GROUPS; ++grp_idx) {
+        if (strcmp(ALL_GROUPS[grp_idx].name, groupname) == 0) {
+            grp = &ALL_GROUPS[grp_idx];
+            break;
+        }
+    }
+    if (grp == NULL) {
+        printf("Group '%s' not found...\n", groupname);
+        return -1;
+    }
+
+    if (grp->n_members < MAX_GROUP_SIZE) {
+        bool member_exist = false;
+        for (size_t i = 0; i < grp->n_members; ++i) {
+            if (strcmp(grp->members[i], username) == 0) {
+                member_exist = true;
+                break;
+            }
+        }
+        if (!member_exist) {
+            // add user
+            grp->join_time[grp->n_members] = time(NULL);
+            strcpy(grp->members[grp->n_members++], username);
+
+            // send old messages
+            int id = get_queue_id(username);
+            
+            for (size_t i = 0; i < (grp->old_msg).n_msg; ++i) {
+                if (grp->join_time[grp->join_time[grp->n_members-1]] <
+                    (grp->old_msg).msg[get_old_msg(&(grp->old_msg), i)].time +
+                    grp->delete_time) {
+                        if (msgsnd(id, (grp->old_msg).msg[get_old_msg(&(grp->old_msg), i)], sizeof(MSG), 0) < 0)
+                            perror("Error in msgsnd...\n");
+                    }
+            }
+            
+            return grp->n_members;
+        }
+        else {
+            printf("Member '%s' already present in group...\n", username);
+            return -1;
+        }
+    }
+    else {
+        printf("Group '%s' full...\n", grp->name);
+        return -1;
+    }
+}
+
+int create_group(char * groupname, char * creator_name) {
+    bool group_exist = false;
+    for (size_t i = 0; i < N_GROUPS; ++i) {
+        if (strcmp(ALL_GROUPS[i].name, groupname) == 0) {
+            group_exist = true;
+            break;
+        }
+    }
+    if (!group_exist) {
+        GROUP grp;
+        memset(&grp, 0, sizeof(GROUP));
+        grp.join_time[0] = time(NULL);
+        strcpy(grp.name, groupname);
+        grp.n_members = 1;
+        strcpy(grp.members[0], creator_name);
+        grp.old_msg.start_ptr = 0;
+        grp.old_msg.n_msg = 0;
+        ++N_GROUPS;
+        ALL_GROUPS[N_GROUPS++] = grp;
+
+        return 0;
+    }
+    return -1;
+}
+
+void list_group() {
+    int id = get_queue_id("server");
+    MSG msg = {0};
+    
+    strcpy(msg.body, "");
+    for (size_t i = 0; i < N_GROUPS; ++i) {
+        strcat(msg.body, ALL_GROUPS[i].name);
+        strcat(msg.body, "\n");
+    }
+    
+    msg.type = LIST_GROUP_MSG;
+    
+    if(msgsnd(id, &msg, sizeof(msg), 0) < 0)
+        perror("Error in msgsnd...\n");
+}
+
+void send_private_msg(MSG * msg) {
+    int id = get_queue_id(msg->sender);
+    if (msgsnd(id, msg, sizeof(MSG), 0) < 0)
+        perror("Error in msgsnd...\n");
+}
+
+int send_group_msg(MSG * msg) {
+    GROUP * grp = NULL;
+    for (size_t grp_idx = 0; grp_idx < N_GROUPS; ++grp_idx) {
+        if (strcmp(ALL_GROUPS[grp_idx].name, msg->sender) == 0) {
+            grp = &ALL_GROUPS[grp_idx];
+            break;
+        }
+    }
+    if (grp == NULL) {
+        printf("Group '%s' not found...\n", msg->sender);
+        return -1;
+    }
+
+    for (size_t ii = 0; ii < grp->n_members; ++ii) {
+        int id = get_queue_id(msg->sender);
+        if (msgsnd(id, msg, sizeof(MSG), 0) < 0)
+            perror("Error in msgsnd...\n");
+    }
+
+    msg->time = time(NULL);
+    add_old_msg(grp->old_msg, msg);
+    return 0;
+}
+
+int main() {
+    N_GROUPS = 0;
+    
+    MSG msg;
+
+    int id = get_queue_id("server");
+
+    while (true) {
+        if (msgrcv(id, &msg, sizeof(msg), 0, 0) < 0)
+            perror("Error in msgrcv...\n");
+
+        if (fork() == 0) {
+            
+        }
+    }
 
 }
