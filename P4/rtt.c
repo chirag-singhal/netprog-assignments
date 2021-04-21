acbef9bdd8cc03a449b2262b93f7667876e472e3
Harshan Baskar <harshan01@gmail.com>
2021-04-21 23:04:57 +0530

    P4 done

--- P4/rtt.c
+++ P4/rtt.c
@@ -14,20 +14,26 @@
 #include <sys/epoll.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip6.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/icmp6.h>
 #include <netdb.h>
 #include <errno.h>
 
+
 #define NUM_MAX_EVENTS  10      // maximum triggered events received from epoll_wait
 #define EPOLL_TIMEOUT   10000   // maximum timeout for epoll_wait in milliseconds
 
 
+int n_complete;
+
 struct host_info {
-    bool valid = false;
+    bool valid;
     char    ip[41];
     bool    isIPv6;
-    float   RTT[3]; // in milliseconds
+    double   RTT[3]; // in milliseconds
+    short int n_rtt_rcvd;
 };
 
 struct pthread_args {
@@ -62,12 +68,18 @@ int send_v4(char ip[41], int epoll_fd) {
     int sock_flags = fcntl(sock_fd, F_GETFL, 0);
     fcntl(sock_fd, F_SETFL, sock_flags | O_NONBLOCK);
 
+    int ttl = 32;
+    if (setsockopt(sock_fd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1)
+        err_exit("Error in setting TTL option. Exiting...\n");
+
     struct sockaddr_in cli_addr = {0};
     // ICMP has no ports
     cli_addr.sin_family = AF_INET;
     if (inet_pton(AF_INET, ip, &cli_addr.sin_addr) == 0)
         err_exit("Invalid IPv4 address. Exiting...\n");
 
+    connect(sock_fd, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+    
     struct epoll_event event;
     event.events = EPOLLIN;
     event.data.fd = sock_fd;
@@ -86,30 +98,30 @@ int send_v4(char ip[41], int epoll_fd) {
     struct icmphdr * icmp = (struct icmphdr *) icmp_payload;
     icmp->type = ICMP_ECHO;
     icmp->code = 0;
-    icmp->un.echo.id = getpid();
+    icmp->un.echo.id = sock_fd;
     icmp->un.echo.sequence = 0;
 
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp->checksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    icmp->checksum = checksum(icmp_payload, icmp_payload_len);
 
     sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
 
     icmp->un.echo.sequence = 1;
     icmp->checksum = 0;
 
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp->checksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    icmp->checksum = checksum(icmp_payload, icmp_payload_len);
 
     sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
 
     icmp->un.echo.sequence = 2;
     icmp->checksum = 0;
 
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp->checksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    icmp->checksum = checksum(icmp_payload, icmp_payload_len);
 
     sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
 
@@ -119,7 +131,7 @@ int send_v4(char ip[41], int epoll_fd) {
 }
 
 int send_v6(char ip[41], int epoll_fd) {
-    int sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    int sock_fd = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
     int sock_flags = fcntl(sock_fd, F_GETFL, 0);
     fcntl(sock_fd, F_SETFL, sock_flags | O_NONBLOCK);
 
@@ -129,6 +141,7 @@ int send_v6(char ip[41], int epoll_fd) {
     if (inet_pton(AF_INET6, ip, &cli_addr.sin6_addr) == 0)
         err_exit("Invalid IPv6 address. Exiting...\n");
     
+    connect(sock_fd, (struct sockaddr_in6 *) &cli_addr, sizeof(struct sockaddr_in6));
     struct epoll_event event;
     event.events = EPOLLIN;
     event.data.fd = sock_fd;
@@ -147,57 +160,59 @@ int send_v6(char ip[41], int epoll_fd) {
     struct icmp6_hdr * icmp6 = (struct icmp6_hdr *) icmp_payload;
     icmp6->icmp6_type = ICMP6_ECHO_REQUEST;
     icmp6->icmp6_code = 0;
-    icmp6->icmp6_id = getpid();
+    icmp6->icmp6_id = sock_fd;
     icmp6->icmp6_seq = 0;
     
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp6 + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp6->icmp6_cksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    // icmp6->icmp6_cksum = htons(checksum(icmp_payload, icmp_payload_len));
 
-    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr_in6 *) &cli_addr, sizeof(struct sockaddr_in6));
 
     icmp6->icmp6_seq = 1;
-    icmp6->icmp6_cksum = 0;
+    // icmp6->icmp6_cksum = htons(0);
 
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp6 + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp6->icmp6_cksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    // icmp6->icmp6_cksum = htons(checksum(icmp_payload, icmp_payload_len));
 
-    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr_in6 *) &cli_addr, sizeof(struct sockaddr_in6));
 
     icmp6->icmp6_seq = 2;
-    icmp6->icmp6_cksum = 0;
+    // icmp6->icmp6_cksum = htons(0);
 
-    clock_gettime(CLOCK_REALTIME, &send_time);
+    clock_gettime(CLOCK_MONOTONIC, &send_time);
     memcpy(icmp6 + 1, &send_time, sizeof(struct timespec)); // Set send time
-    icmp6->icmp6_cksum = checksum((void *) &icmp_payload, icmp_payload_len);
+    // icmp6->icmp6_cksum = htons(checksum(icmp_payload, icmp_payload_len));
 
-    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+    sendto(sock_fd, icmp_payload, icmp_payload_len, 0, (struct sockaddr_in6 *) &cli_addr, sizeof(struct sockaddr_in6));
 
     free(icmp_payload);
 
     return sock_fd;
 }
 
-void receive_reply_func(struct pthread_args * args) {
+void * receive_reply_func(void * args) {
     // Waiting for ICMP echo replies from all IPs using epoll
 
     // Copy pthread args
-    struct host_info * hosts = args->hosts;
-    size_t num_hosts = args->num_hosts;
-    int epoll_fd = args->epoll_fd;
+    struct host_info * hosts = ((struct pthread_args *)args)->hosts;
+    size_t num_hosts = ((struct pthread_args *)args)->num_hosts;
+    int epoll_fd = ((struct pthread_args *)args)->epoll_fd;
 
     struct epoll_event trig_events[NUM_MAX_EVENTS];
 
     struct timespec recv_time, send_time;
-    size_t icmp_payload_len = sizeof(struct timespec) + ((sizeof(struct icmphdr) > sizeof(struct icmp6_hdr)) ?
-                                sizeof(struct icmphdr) : sizeof(struct icmp6_hdr));
-    void * icmp_payload = malloc(icmp_payload_len);
+    size_t icmp_payload_len = sizeof(struct timespec) +
+                                ((sizeof(struct icmphdr) > sizeof(struct icmp6_hdr)) ? sizeof(struct icmphdr) : sizeof(struct icmp6_hdr));
+    void * ip_icmp_payload = malloc(sizeof(struct iphdr) + icmp_payload_len);
+    void * icmp_payload;
 
-    while (true) {
+    bool run_flag = true;
+    while (run_flag) {
         // Create a new thread to handle searching and updating the hosts data
         int event_count = epoll_wait(epoll_fd, trig_events, NUM_MAX_EVENTS, 10000);
-        clock_gettime(CLOCK_REALTIME, &recv_time); // get receive time
+        clock_gettime(CLOCK_MONOTONIC, &recv_time); // get receive time
         
         if (event_count == 0) {
             printf("Maximum epoll wait timeout reached!\n");
@@ -207,42 +222,75 @@ void receive_reply_func(struct pthread_args * args) {
             err_exit("Error in epoll wait. Exiting...\n");
         
         for (int i = 0; i < event_count; ++i) {
-            if (!hosts[trig_events.data.fd].isIPv6) {
+            struct epoll_event * event = &(trig_events[i]);
+            if (!hosts[event->data.fd].isIPv6) {
                 // Receive IPv4 ICMP echo reply
-                recvfrom(trig_events.data.fd, icmp_payload, icmp_payload_len, 0, NULL, NULL);
-                short int rtt_idx = ((struct icmphdr *) &icmp_payload)->un.echo.sequence;
-                send_time = *((struct timespec *) (((struct icmphdr *) &icmp_payload)+1));
+                recvfrom(event->data.fd, ip_icmp_payload, sizeof(struct iphdr) + icmp_payload_len, 0, NULL, NULL);
+                
+                icmp_payload = ((struct iphdr *) ip_icmp_payload) + 1;
+                if (((struct icmphdr *) icmp_payload)->type == ICMP_ECHOREPLY) {
+                    short int rtt_idx = ((struct icmphdr *) icmp_payload)->un.echo.sequence;
+                    send_time = *((struct timespec *) (((struct icmphdr *) icmp_payload)+1));
 
-                // Time difference for RTT in milliseconds
-                float rtt = ((float)(recv_time.tv_sec - send_time.tv_sec)*1000.0) + (float)(recv_time.tv_nsec - send_time.tv_nsec)/1000.0;
-                hosts[trig_events.data.fd].RTT[rtt_idx] = rtt;
+                    // Time difference for RTT in milliseconds
+                    double rtt = ((double)(recv_time.tv_sec - send_time.tv_sec)*1000.0) + (double)(recv_time.tv_nsec - send_time.tv_nsec)/1000000.0;
+                    hosts[event->data.fd].RTT[rtt_idx] = rtt;
+                    if (++hosts[event->data.fd].n_rtt_rcvd == 3) {
+                        printf("%-41s - %.2f %.2f %.2f\t\t(ms)\n", hosts[event->data.fd].ip,
+                            hosts[event->data.fd].RTT[0],
+                            hosts[event->data.fd].RTT[1],
+                            hosts[event->data.fd].RTT[2]);
+                        close(event->data.fd);
+                        ++n_complete;
+                        if (n_complete == num_hosts)
+                            run_flag = false;
+                    }
+                }
             }
             else {
                 // Receive IPv6 ICMP echo reply
-                recvfrom(trig_events.data.fd, icmp_payload, icmp_payload_len, 0, NULL, NULL);
-                short int rtt_idx = ((struct icmp6_hdr *) &icmp_payload)->icmp6_seq;
-                send_time = *((struct timespec *) (((struct icmp6_hdr *) &icmp_payload)+1));
+                recvfrom(event->data.fd, ip_icmp_payload, icmp_payload_len, 0, NULL, NULL);
+                
+                icmp_payload = ip_icmp_payload;
+                if (((struct icmp6_hdr *) icmp_payload)->icmp6_type == ICMP6_ECHO_REPLY) {
+                    short int rtt_idx = ((struct icmp6_hdr *) icmp_payload)->icmp6_seq;
+                    send_time = *((struct timespec *) (((struct icmp6_hdr *) icmp_payload)+1));
 
-                // Time difference for RTT in milliseconds
-                float rtt = ((float)(recv_time.tv_sec - send_time.tv_sec)*1000.0) + (float)(recv_time.tv_nsec - send_time.tv_nsec)/1000.0;
-                hosts[trig_events.data.fd].RTT[rtt_idx] = rtt;
+                    // Time difference for RTT in milliseconds
+                    double rtt = ((double)(recv_time.tv_sec - send_time.tv_sec)*1000.0) + (double)(recv_time.tv_nsec - send_time.tv_nsec)/1000000.0;
+                    hosts[event->data.fd].RTT[rtt_idx] = rtt;
+                    if (++hosts[event->data.fd].n_rtt_rcvd == 3) {
+                        printf("%-41s - %.2f %.2f %.2f\t\t(ms)\n", hosts[event->data.fd].ip,
+                            hosts[event->data.fd].RTT[0],
+                            hosts[event->data.fd].RTT[1],
+                            hosts[event->data.fd].RTT[2]);
+                        close(event->data.fd);
+                        ++n_complete;
+                        if (n_complete == num_hosts)
+                            run_flag = false;
+                    }
+                }
             }
         }
     }
-
-    for (int i = 0; i < num_hosts; ++i) {
-        printf("%s - %f %f %f\n", hosts)
-    }
     
-    free(icmp_payload);
+    free(ip_icmp_payload);
+
+    // Output of thread. In this case, there is nothing to return
+    return NULL;
 }
 
 int main(int argc, char * argv[]) {
 
+    if (argc < 2)
+        err_exit("HOSTS IP Filename argument not specified! Exiting...\n");
+
+    // Assumption: All the hosts in the file are unique
     size_t num_hosts = 0;
+    n_complete = 0;
     
     FILE * hosts_fp;
-    if ((hosts_fp = fopen(HOSTS_FILE, "r")) == NULL)
+    if ((hosts_fp = fopen(argv[1], "r")) == NULL)
         err_exit("Error opening HOSTS FILE. Exiting...\n");
 
     int _c;
@@ -256,7 +304,7 @@ int main(int argc, char * argv[]) {
     // because 0 and 1 are used up by stdin and stdout
     // So, safety limit has to be atleast +2
     struct host_info * hosts = malloc(sizeof(struct host_info) * (num_hosts + 10));
-    memset(host_info, 0, sizeof(struct host_info) * num_hosts);
+    memset(hosts, 0, sizeof(struct host_info) * num_hosts);
 
     // size_t num_forks = 0;
     int epoll_fd = epoll_create1(0);
@@ -264,7 +312,7 @@ int main(int argc, char * argv[]) {
         err_exit("Error in epoll. Exiting...\n");
 
     pthread_t thread_id;
-    pthread_args args = {hosts, num_hosts, epoll_fd};
+    struct pthread_args args = {hosts, num_hosts, epoll_fd};
     pthread_create(&thread_id, NULL, receive_reply_func, &args);
     
     // Sending ICMP echo requests to all IPs
@@ -273,20 +321,22 @@ int main(int argc, char * argv[]) {
         fgets(ip, 41, hosts_fp);
         if (ip[strlen(ip) - 1] == '\n')
             ip[strlen(ip) - 1] = '\0';
-        
+
+        int sock_fd;
         // Differentiate between ipv4 and ipv6
         if (strstr(ip, ".")) {
             // ipv4 address
-            int sock_fd = send_v4(hosts[idx].ip, epoll_fd);
+            sock_fd = send_v4(ip, epoll_fd);
             hosts[sock_fd].isIPv6 = 0;
         }
         else {
             // ipv6 address
-            int sock_fd = send_v6(hosts[idx].ip, epoll_fd);
+            sock_fd = send_v6(ip, epoll_fd);
             hosts[sock_fd].isIPv6 = 1;
         }
         hosts[sock_fd].valid = true;
         strncpy(hosts[sock_fd].ip, ip, 41);
+        hosts[sock_fd].n_rtt_rcvd = 0;
     }
 
     pthread_join(thread_id, NULL);
